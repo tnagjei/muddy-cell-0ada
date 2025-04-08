30b3546d34ea2fcf6e0876cd95f145b402a0c279
tnagjei <132260476+tnagjei@users.noreply.github.com>
2025-04-08 18:27:13 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
